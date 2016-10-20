b70975501effe36beb20a75951545c63679fc0f5
le-du6 <benoit.leger@stelau.com>
2016-10-20 19:35:32 +0200

    avancement

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import {Router, Route, Link, hashHistory, browserHistory} from 'react-router';
 
 const Home = () => <div><h2>Home</h2><Links /><br/></div>;
 const About = () => <div><h2>About</h2><Links /><br/></div>;
-const Contact = () => <div><h2>Contact</h2><Links /><br/></div>;
+const Contact = (props) => <div><h2>Contact</h2><Links /><br/></div>;
+const Us = () => <div><h2>Us</h2><Links /><br/></div>;
 
-const Links = () =>
+const Links = () => (
   <nav>
-    <Link to='/'>Home</Link><br/>
-    <Link to='/contact'>Contact</Link><br/>
-    <Link to='/about'>About</Link><br/>
+    <Link activeClassName='active' to='/'>Home</Link><br/>
+    <Link activeClassName='active' to='/contact'>Contact</Link><br/>
+    <Link activeClassName='active' to='/about'>About</Link><br/>
   </nav>
+);
 
 class App extends Component {
   render() {
     return (
       <div>
         <Router history={hashHistory}>
-          <Route path="/" component={Home}></Route>
-          <Route path="/contact" component={Contact}></Route>
+          <Route path="/" component={Home}>
+          </Route>
+          <Route path="/contact" component={Contact}>
+            <Route path="us" component={Us}></Route>
+          </Route>
           <Route path="/about" component={About}></Route>
         </Router>        
       </div>
